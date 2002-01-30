f8015de1573209cb3bbbb35f012a3d1f1d2b71f5
Denton Woods <devil.imagelib@gmail.com>
2002-01-30 20:52:55 +0000

    Added to CVS tree

---
+++ DevIL/src-IL/include/il_psd.h
@@ -0,0 +1,55 @@
+//-----------------------------------------------------------------------------
+//
+// ImageLib Sources
+// Copyright (C) 2000-2001 by Denton Woods
+// Last modified: 01/23/2001 <--Y2K Compliant! =]
+//
+// Filename: openil/il_psd.c
+//
+// Description: Reads from a PhotoShop (.psd) file.
+//
+//-----------------------------------------------------------------------------
+
+
+#ifndef PSD_H
+#define PSD_H
+
+#include "il_internal.h"
+
+#ifdef _MSC_VER
+#pragma pack(push, packed_struct, 1)
+#endif
+typedef struct PSDHEAD
+{
+	ILubyte		Signature[4];
+	ILushort	Version;
+	ILubyte		Reserved[6];
+	ILushort	Channels;
+	ILuint		Height;
+	ILuint		Width;
+	ILushort	Depth;
+	ILushort	Mode;
+} IL_PACKSTRUCT PSDHEAD;
+
+#ifdef _MSC_VER
+#pragma pack(pop,  packed_struct)
+#endif
+
+ILushort	Channels;
+
+ILboolean	iIsValidPsd(ILvoid);
+ILboolean	iCheckPsd(PSDHEAD *Header);
+ILboolean	iLoadPsdInternal(ILvoid);
+ILboolean	ReadPsd(PSDHEAD *Head);
+ILboolean	ReadGrey(PSDHEAD *Head);
+ILboolean	ReadIndexed(PSDHEAD *Head);
+ILboolean	ReadRGB(PSDHEAD *Head);
+ILboolean	ReadCMYK(PSDHEAD *Head);
+ILuint		*GetCompChanLen(PSDHEAD *Head);
+ILboolean	GetData(PSDHEAD *Head, ILvoid *Buffer, ILboolean Compressed);
+ILboolean	ParseResources(ILuint ResourceSize, ILubyte *Resources);
+ILboolean	GetSingleChannel(PSDHEAD *Head, ILubyte *Buffer, ILboolean Compressed);
+
+
+
+#endif//PSD_H
