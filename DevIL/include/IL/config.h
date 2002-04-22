b191969cb8af677266d4b2d64f55742063aca99d
Denton Woods <devil.imagelib@gmail.com>
2002-04-22 08:05:55 +0000

    *** empty log message ***

---
+++ DevIL/include/IL/config.h
@@ -0,0 +1,15 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+//#define IL_NO_JPG
+//#define IL_NO_MNG
+//#define IL_NO_PNG
+//#define IL_NO_TIF
+//#define IL_NO_GIF
+//#define IL_NO_LCMS
+
+#define ILUT_USE_OPENGL
+#define ILUT_USE_DIRECTX8
+#define ILUT_USE_WIN32
+
+#endif /* __CONFIG_H__ */
