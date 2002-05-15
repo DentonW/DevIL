9e0f78d51f076b4175030cc96221d2272c77a961
Denton Woods <devil.imagelib@gmail.com>
2002-05-15 01:11:47 +0000

    il_gif.h addition

---
+++ DevIL/src-IL/include/il_gif.h
@@ -0,0 +1,54 @@
+//-----------------------------------------------------------------------------
+//
+// ImageLib Sources
+// Copyright (C) 2000-2002 by Denton Woods
+// Last modified: 05/13/2002 <--Y2K Compliant! =]
+//
+// Filename: il/il_gif.h
+//
+// Description: Reads from a Graphics Interchange Format (.gif) file.
+//
+//-----------------------------------------------------------------------------
+
+
+#ifndef GIF_H
+#define GIF_H
+
+#include "il_internal.h"
+
+#define GIF87A 87
+#define GIF89A 89
+
+#ifdef _WIN32
+	#pragma pack(push, gif_struct, 1)
+#endif
+typedef struct GIFHEAD
+{
+	char		Sig[6];
+	ILushort	Width;
+	ILushort	Height;
+	ILubyte		ColourInfo;
+	ILubyte		Background;
+	ILubyte		Aspect;
+} IL_PACKSTRUCT GIFHEAD;
+
+typedef struct IMAGEDESC
+{
+	ILubyte		Separator;
+	ILushort	OffX;
+	ILushort	OffY;
+	ILushort	Width;
+	ILushort	Height;
+	ILubyte		ImageInfo;
+} IL_PACKSTRUCT IMAGEDESC;
+#ifdef _WIN32
+	#pragma pack(pop, gif_struct)
+#endif
+
+// Internal functions
+ILboolean iLoadGifInternal(ILvoid);
+ILboolean ilLoadGifF(ILHANDLE File);
+ILboolean iIsValidGif(ILvoid);
+ILboolean GifGetData(ILvoid);
+
+#endif//GIF_H
