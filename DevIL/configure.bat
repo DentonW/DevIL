7b33b555ee974df490dab4a8a96418d0acf5258c
Denton Woods <devil.imagelib@gmail.com>
2002-05-15 19:04:54 +0000

    Added configure.bat

---
+++ DevIL/configure.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Generating include/IL/config.h based on what your system has installed...
+"LIBTOOLS\config gen\config_gen.exe" include\IL
+echo Done.
+echo.
+echo Please check include/IL/config.h to make sure that it has
+echo  set the appropriate #defines.
+echo.
+pause
