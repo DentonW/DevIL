ac67ce8d8fb648280cef4d9dd4458085a292d137
Nicolas Weber <thakis@users.sourceforge.net>
2005-05-07 22:15:47 +0000

    Added support for compressed textures on linux

--- DevIL/src-ILUT/include/ilut_opengl.h
+++ DevIL/src-ILUT/include/ilut_opengl.h
@@ -32,7 +32,11 @@ ILboolean IsExtensionSupported(const char *extension);
 #ifdef _WIN32
 	#include <windows.h>
 	typedef void (ILAPIENTRY * ILGLCOMPRESSEDTEXIMAGE2DARBPROC) (GLenum target, GLint level, GLenum internalformat, GLsizei width, GLsizei height, GLint border, GLsizei imageSize, const GLvoid *data);
+#elif __linux__
+	#include <GL/gl.h>
+	typedef void (ILAPIENTRY * ILGLCOMPRESSEDTEXIMAGE2DARBPROC) (GLenum target, GLint level, GLenum internalformat, GLsizei width, GLsizei height, GLint border, GLsizei imageSize, const GLvoid *data);
 #endif
+
 
 #endif ILUT_USE_OPENGL
 #endif//ILUT_OPENGL_H
