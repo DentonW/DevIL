cb351f773545aecf9816c6a91ba5c092c1838691
Meloni Dario <darkyojimbo@users.sourceforge.net>
2006-07-05 16:55:30 +0000

    Now useless

--- DevIL/src-IL/src/il_inline.c
+++
@@ -1,7 +0,0 @@
-
-// file used to generate the non external declaration of the inline function
-//#if defined(__GNUC__)
-	#define NOINLINE 1
-	#include "il_internal.h"
-	#include "il_endian.h"
-//#endif
