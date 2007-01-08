92c4aa7df9f23d6d97f59bc74f167a44925e3735
Meloni Dario <darkyojimbo@users.sourceforge.net>
2007-01-08 11:37:38 +0000

    Some autogen modifications

--- DevIL/autogen.sh
+++ DevIL/autogen.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
-#
-aclocal -I .
-autoheader
-automake --foreign --include-deps --add-missing --copy
-autoconf
+# Author: Meloni Dario <mellon85 -at- gmail.com>
 
-#./configure $*
-#echo "Now you are ready to run ./configure"
+# use the -v flag to get all the output
+if [ "$1" = "-v" ] ; then
+	VERBOSE=yes
+fi
+
+log() {
+	if [ "$VERBOSE" = "yes" ] ; then
+		$1
+		output=""
+	else
+		output=$($1 2>&1)
+	fi
+	
+	# show output only in case of errors
+	if [ $? -ne 0 ] ; then
+		echo $output
+		exit 1
+	fi
+}
+
+log "aclocal -I ."
+log "autoheader"
+log "automake --foreign --include-deps --add-missing --copy"
+log "autoconf"
+exit 0
