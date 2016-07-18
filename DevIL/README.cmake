258114e5cc6515182243e8397a7f3215fb463fe8
Ben Campbell <ben@scumways.com>
2016-07-18 21:11:49 +1200

    Remove various build systems in favour of CMake
    This removes support for GNU autotools, as well as a whole bunch of
    IDE project files.
    The plan is to consolidate effort behind a single build system rather
    that trying to maintain a bunch of them. And CMake seems to be the
    best supported across the various target platforms.
    
    The old 'projects' directory contained a bunch of language bindings.
    Those have been moved into a new directory, 'bindings'.

--- DevIL/README.cmake
+++ DevIL/README.cmake
@@ -10,18 +10,28 @@ For Linux/Unix/OSX/mingw, using makefiles:
     $ make
     $ make install
 
-To generate visual studio project files (untested!):
+To generate project files for visual studio/xcode/whatever, you
+need to tell CMake which generator to use. For example:
 
-    makedir build
-    cd build
     cmake -G"Visual Studio 14 2015" ..
 
 See here for list of generators:
 http://www.cmake.org/cmake/help/v3.3/manual/cmake-generators.7.html
 
 
-
 Debug/Release/etc:
 
     $ cmake -DCMAKE_BUILD_TYPE=Debug ..
+
+
+TODO:
+- windows: document any linkage issues with Visual Studio - multithreaded,
+  unicode, whatever.
+- OSX: document any OSX-specificness - eg can we produce framework bundles
+  easily?
+- give some hints on installing prerequisites, eg:
+    - an apt-get install line for ubuntu
+    - brew packages to install for OSX
+    - instructions for Msys2 on windows
+    - steps for setting up libs for visual studio
 
