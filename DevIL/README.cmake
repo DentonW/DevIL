f8fc86cb4d4994e81fe6b4dbd001fa972f40cb9e
Ben Campbell <ben@scumways.com>
2016-11-13 15:35:13 +1300

    add some notes on building under visual studio

--- DevIL/README.cmake
+++ DevIL/README.cmake
@@ -1,6 +1,8 @@
 Build DevIL with CMake
 ----------------------
 
+GENERIC
+-------
 
 For Linux/Unix/OSX/mingw, using makefiles:
 
@@ -14,7 +16,6 @@ For Linux/Unix/OSX/mingw, using makefiles:
 To generate project files for visual studio/xcode/whatever, you
 need to tell CMake which generator to use. For example:
 
-    cmake -G"Visual Studio 14 2015" ..
 
 See here for list of generators:
 http://www.cmake.org/cmake/help/v3.3/manual/cmake-generators.7.html
@@ -25,11 +26,49 @@ Debug/Release/etc:
     $ cmake -DCMAKE_BUILD_TYPE=Debug ..
 
 
+WINDOWS - Visual studio
+-----------------------
+
+You can get going without any commandline fiddling. Just install cmake
+(from http://cmake.org), and then:
+
+1) Run cmake-gui.
+
+2) Set the "where is the source code:" entry to the dir containing the top-level
+CMakeLists.txt, which should be DevIL/DevIL.
+
+3) Set a build dir with the "where to build the binaries:" entry. Generally, this
+will be a new directory outside of the DevIL tree (to keep the build all clean
+and out-of-tree).
+
+4) click the "configure" button. It should let you specify the generator to use.
+eg:
+  "Visual Studio 14 2015" (for a 32-buit build)
+  "Visual Studio 14 2015 w64" (for a 64-buit build)
+It'll spend a little time generating the project files and then you're set to go.
+
+5) In visual studio, navigate to the build directory and open up the new .sln file.
+
+6) build!
+Configurations for both Debug and Release (and others) are included in the project.
+
+
+WINDOWS - msys2
+---------------
+
+
+Run mingw32 or mingw64 shell (for 32bit or 64bit respectively), then follow
+the generic instructions, except for:
+
+  $ cmake -G "MSYS Makefiles" ..
+
+To set install prefix (windows default seems to be C:/program files/....):
+  $ cmake -G "MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=/usr/local ..
+
+
+
+
 TODO:
-- windows: document any linkage issues with Visual Studio - multithreaded,
-  unicode, whatever.
-- OSX: document any OSX-specificness - eg can we produce framework bundles
-  easily?
 - give some hints on installing prerequisites, eg:
     - an apt-get install line for ubuntu
     - brew packages to install for OSX
@@ -38,10 +77,4 @@ TODO:
 
 
 
-Building with msys2 and mingw32/mingw64:
-
-  $ cmake -G "MSYS Makefiles" ..
-
-To set install prefix (windows default seems to be C:/program files/....):
-  $ cmake -G "MSYS Makefiles" -DCMAKE_INSTALL_PREFIX=/usr/local ..
 
